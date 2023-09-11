bced456ae8bab0d351e650cac9ea2f80f42757a1
Taru <7aru.pelton3n@gmail.com>
2023-09-11 13:48:15 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import RadioForm from 'react-native-simple-radio-button';
+import { TextInput, View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  // State variables weight, intensity, gender and calculated calorie count
+  const [weight, setWeight] = useState(0);
+  const [intensity, setIntensity] = useState(1.3);
+  const [gender, setGender] = useState('male');
+  const [calories, setCalories] = useState(0);
+
+  // Selection options for intensities and genders
+  const intensities = Array();
+  intensities.push({ label: 'light', value: 1.3 });
+  intensities.push({ label: 'usual', value: 1.5 });
+  intensities.push({ label: 'moderate', value: 1.7 });
+  intensities.push({ label: 'hard', value: 2 });
+  intensities.push({ label: 'very hard', value: 2.2 });
+
+  const genders = [
+    { label: 'Male', value: 'male' },
+    { label: 'Female', value: 'female' }
+  ];
+
+  // Function for counting calories
+  const calculate = () => {
+    let result = 0;
+    if (gender === 'male') {
+      result = (879 + 10.2 * weight) * intensity;
+    } else {
+      result = (795 + 7.18 * weight) * intensity;
+    }
+    setCalories(result);
+  };
+
+  // Defining styles
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 56,
+      margin: 8,
+    },
+    field: {
+      marginBottom: 8,
+      marginTop: 8,
+    },
+    radio: {
+      marginTop: 8,
+    },
+    intensity: {
+      alignSelf: 'stretch',
+    }
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Weight</Text>
+      <View style={styles.field}>
+        <TextInput
+          onChangeText={text => setWeight(text)}
+          placeholder='in kilograms' // Empty input field
+          keyboardType='number-pad' // Keyboard type
+        />
+      </View>
+      <View style={styles.field}>
+        <Text>Intensity</Text>
+        <Picker
+          style={styles.intensity}
+          onValueChange={(itemValue) => setIntensity(itemValue)}
+          selectedValue={intensity}
+        >
+          {intensities.map((intensity, index) => (
+            <Picker.Item key={index} label={intensity.label} value={intensity.value} />
+          ))}
+        </Picker>
+      </View>
+      <View style={styles.field}>
+  <Text>Gender</Text>
+  <RadioForm
+    style={styles.radio}
+    buttonSize={10}
+    radio_props={genders} // Selection options
+    initial={0} // Initialized selection
+    onPress={value => setGender(value)}
+  />
+</View>
+<View style={styles.field}>
+  <Text>{calories.toFixed(0)}</Text>
+</View>
+<Button title="Calculate" onPress={calculate} /> 
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
